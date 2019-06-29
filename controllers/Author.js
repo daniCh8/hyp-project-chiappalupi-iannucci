6fea18467c9c7af3ca2dea940f0f8e48c0846778
Daniele Chiappalupi <daniele.chiappalupi@mail.polimi.it>
2019-06-29 12:39:27 +0200

    added 404 not found in getAuthorByID

--- controllers/Author.js
+++ controllers/Author.js
@@ -37,19 +37,11 @@ module.exports.deleteAuthor = function deleteAuthor (req, res, next) {
 
 module.exports.getAuthorByID = function getAuthorByID (req, res, next) {
   var ID = req.swagger.params['ID'].value;
-  //console.log("ma qui ci arrivo?")
   Author.getAuthorByID(ID)
     .then(function (response) {
-      /*console.log("oh zio siamo qui")
       var responseCode = 200
-      console.log("oh zio responseCode1")
-      console.log(responseCode)
-      console.log("oh zio response")
-      console.log(response)
-      console.log("oh zio response.length")
-      console.log(response.length)
-      if(response.length == 0) responseCode = 404*/
-      utils.writeJson(res, response/*, responseCode*/);
+      if(response.length == 0) responseCode = 404
+      utils.writeJson(res, response, responseCode);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
