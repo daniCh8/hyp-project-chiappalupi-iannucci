b9eb575089478b555db3c0ef0bbadc4dc4213272
Daniele Chiappalupi <daniele.chiappalupi@mail.polimi.it>
2019-06-29 12:26:01 +0200

    added ID check in getAuthorByID

--- controllers/Author.js
+++ controllers/Author.js
@@ -39,7 +39,16 @@ module.exports.getAuthorByID = function getAuthorByID (req, res, next) {
   var ID = req.swagger.params['ID'].value;
   Author.getAuthorByID(ID)
     .then(function (response) {
-      utils.writeJson(res, response);
+      console.log("oh zio siamo qui")
+      var responseCode = 200
+      console.log("oh zio responseCode1")
+      console.log(responseCode)
+      console.log("oh zio response")
+      console.log(response)
+      console.log("oh zio response.length")
+      console.log(response.length)
+      if(response.length == 0) responseCode = 404
+      utils.writeJson(res, response, responseCode);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
