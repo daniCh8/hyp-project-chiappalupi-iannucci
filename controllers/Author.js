3c2247ff0ac0fbf47ba7fb8a06b8f09ba2d8b3ad
Daniele Chiappalupi <daniele.chiappalupi@mail.polimi.it>
2019-06-30 11:26:40 +0200

    fixed /author/{ID} DELETE api documentation

--- controllers/Author.js
+++ controllers/Author.js
@@ -3,76 +3,87 @@
 var utils = require('../utils/writer.js');
 var Author = require('../service/AuthorService');
 
-module.exports.getAuthors = function getAuthors (req, res, next) {
-  Author.getAuthors()
-    .then(function (response) {
-      var responseCode = 200
-      if(response.length == 0) {
-        responseCode = 404
-        response = {
-          "success": false,
-          "errorMessage": "No authors found in the database"
+module.exports.getAuthors = function getAuthors(req, res, next) {
+    Author.getAuthors()
+        .then(function(response) {
+            var responseCode = 200
+            if (response.length == 0) {
+                responseCode = 404
+                response = {
+                    "success": false,
+                    "errorMessage": "No authors found in the database"
+                }
+            }
+            utils.writeJson(res, response, responseCode);
+        })
+        .catch(function(response) {
+            utils.writeJson(res, response);
+        });
+};
+
+module.exports.addAuthor = function addAuthor(req, res, next) {
+    var body = req.swagger.params['body'].value;
+    Author.addAuthor(body)
+        .then(function(response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function(response) {
+            utils.writeJson(res, response);
+        });
+};
+
+module.exports.deleteAuthor = function deleteAuthor(req, res, next) {
+    var ID = req.swagger.params['ID'].value;
+    var responseCode = 200;
+    Author.getAuthorByID(ID).then(function(response) {
+        if (response.length == 0) {
+            responseCode = 404
+            response = {
+                "success": false,
+                "errorMessage": "No authors with this ID found in the database."
+            }
+            utils.writeJson(res, response, responseCode);
+        } else {
+            Author.deleteAuthor(ID)
+                .then(function(response) {
+                    utils.writeJson(res, response, responseCode);
+                })
+                .catch(function(response) {
+                    utils.writeJson(res, response);
+                });
         }
-      }
-      utils.writeJson(res, response, responseCode);
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
 };
 
-module.exports.addAuthor = function addAuthor (req, res, next) {
-  var body = req.swagger.params['body'].value;
-  Author.addAuthor(body)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.getAuthorByID = function getAuthorByID(req, res, next) {
+    var ID = req.swagger.params['ID'].value;
+    Author.getAuthorByID(ID)
+        .then(function(response) {
+            var responseCode = 200
+            if (response.length == 0) {
+                responseCode = 404
+                response = {
+                    "success": false,
+                    "errorMessage": "No authors with this ID found in the database."
+                }
+            } else {
+                var box = response[0]
+                response = box
+            }
+            utils.writeJson(res, response, responseCode);
+        })
+        .catch(function(response) {
+            utils.writeJson(res, response);
+        });
 };
 
-module.exports.deleteAuthor = function deleteAuthor (req, res, next) {
-  var ID = req.swagger.params['ID'].value;
-  Author.deleteAuthor(ID)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.getAuthorByID = function getAuthorByID (req, res, next) {
-  var ID = req.swagger.params['ID'].value;
-  Author.getAuthorByID(ID)
-    .then(function (response) {
-      var responseCode = 200
-      if(response.length == 0) {
-        responseCode = 404
-        response = {
-          "success": false,
-          "errorMessage": "No authors with this ID found in the database."
-        }
-      }
-      else {
-        var box = response[0]
-        response = box
-      }
-      utils.writeJson(res, response, responseCode);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.findAuthorsByName = function findAuthorsByName (req, res, next) {
-  var names = req.swagger.params['name'].value;
-  Author.findAuthorsByName(names)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.findAuthorsByName = function findAuthorsByName(req, res, next) {
+    var names = req.swagger.params['name'].value;
+    Author.findAuthorsByName(names)
+        .then(function(response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function(response) {
+            utils.writeJson(res, response);
+        });
 };
