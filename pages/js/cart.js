16b8729cdfa276762c316f7f7d4f1ca723ba9a0d
Elena Iannucci <elena.iannucci@mail.polimi.it>
2019-07-04 15:33:23 +0200

    This will work, I promise

--- pages/js/cart.js
+++ pages/js/cart.js
@@ -140,10 +140,9 @@ var ISBN = data[0].ISBN;
 
 }
 
-
 function drawQuantity(data){
     for(var i = 0; i<data.length; i++){
-        var s = '<div class = "container col-12">' +
+        var s = '<div class = "container col-12 elementContainer fadeIn">' +
             '<div class="container col-6" id="'+ data[i].ISBN +'"></div> ' +
             '<div class="container col-6" <p class="paragraph-fav-best">Quantity:'+data[i].quantity +'</p>'+
         '</div>'+
