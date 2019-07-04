d543deefc66a23751602c307ba69da7e76cd8667
Elena Iannucci <elena.iannucci@mail.polimi.it>
2019-07-04 16:05:48 +0200

    Updated login page

--- pages/js/myaccount.js
+++ pages/js/myaccount.js
@@ -198,56 +198,52 @@ function drawAccount(data) {
 
 function drawLoginForm() {
     var s = '';
-    s = s + '<div class="sidenav">\n' +
-        '    <div class="login-main-text">\n' +
-        '        <h2>Login Page</h2>\n' +
-        '        <p>Login or register from here to access.</p>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<div class="main">\n' +
-        '    <div class="col-6">\n' +
-        '        <div class="login-form loginContainer">\n' +
-        '            <p class="paragraph-fav-best">Already registered? Login now!</p>\n' +
-        '            <form>\n' +
-        '                <div class="form-group">\n' +
-        '                    <label>Username</label>\n' +
-        '                    <input type="text" class="form-control" id="username" placeholder="Username">\n' +
+    s = s + '<div class="login-html">\n' +
+        '        <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>\n' +
+        '        <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>\n' +
+        '        <div class="login-form">\n' +
+        '            <div class="sign-in-htm">\n' +
+        '                <div class="group">\n' +
+        '                    <label for="username" class="label">Username</label>\n' +
+        '                    <input id="username" type="text" class="input">\n' +
         '                </div>\n' +
-        '                <div class="form-group">\n' +
-        '                    <label>Password</label>\n' +
-        '                    <input type="password" class="form-control" id="password" placeholder="Password">\n' +
+        '                <div class="group">\n' +
+        '                    <label for="password" class="label">Password</label>\n' +
+        '                    <input id="password" type="password" class="input" data-type="password">\n' +
         '                </div>\n' +
-        //'                <button type="button" class="btn btn-black" id="loginButton">Login</button>\n' +
-        '            </form>\n' +
+        '                <div class="group">\n' +
+        '                    <input type="submit" class="button" value="Sign In">\n' +
+        '                </div>\n' +
+        '                <div class="hr"></div>\n' +
+        '\n' +
+        '            </div>\n' +
+        '            <div class="sign-up-htm">\n' +
+        '                <div class="group">\n' +
+        '                    <label for="firstname" class="label">First Name</label>\n' +
+        '                    <input id="firstname" type="text" class="input">\n' +
+        '                </div>\n' +
+        '                <div class="group">\n' +
+        '                    <label for="lastname" class="label">Last Name</label>\n' +
+        '                    <input id="lastname" type="text" class="input">\n' +
+        '                </div>\n' +
+        '                <div class="group">\n' +
+        '                    <label for="usernameReg" class="label">Username</label>\n' +
+        '                    <input id="usernameReg" type="text" class="input">\n' +
+        '                </div>\n' +
+        '                <div class="group">\n' +
+        '                    <label for="email" class="label">Email</label>\n' +
+        '                    <input id="email" type="text" class="input">\n' +
+        '                </div>\n' +
+        '                <div class="group">\n' +
+        '                    <label for="passwordReg" class="label">Password</label>\n' +
+        '                    <input id="passwordReg" type="password" class="input" data-type="password">\n' +
+        '                </div>\n' +
+        '                <div class="group">\n' +
+        '                    <input type="submit" class="button" value="Sign Up">\n' +
+        '                </div>\n' +
+        '                <div class="hr"></div>\n' +
+        '            </div>\n' +
         '        </div>\n' +
-        '    </div>\n' +
-        '    <div class="col-6">\n' +
-        '    <div class="login-form loginContainer">\n' +
-        '        <p class="paragraph-fav-best">New user? Register now!</p>\n' +
-        '        <form>\n' +
-        '            <div class="form-group">\n' +
-        '                <label>First Name</label>\n' +
-        '                <input type="text" class="form-control" id="firstname" placeholder="First Name">\n' +
-        '            </div>\n' +
-        '            <div class="form-group">\n' +
-        '                <label>Last Name</label>\n' +
-        '                <input type="text" class="form-control" id="lastname" placeholder="Last Name">\n' +
-        '            </div>\n' +
-        '            <div class="form-group">\n' +
-        '                <label>E-mail</label>\n' +
-        '                <input type="text" class="form-control" id="email" placeholder="E-mail">\n' +
-        '            </div>\n' +
-        '            <div class="form-group">\n' +
-        '                <label>Username</label>\n' +
-        '                <input type="text" class="form-control" id="usernameReg" placeholder="Username">\n' +
-        '            </div>\n' +
-        '            <div class="form-group">\n' +
-        '                <label>Password</label>\n' +
-        '                <input type="password" class="form-control" id="passwordReg" placeholder="Password">\n' +
-        '            </div>\n' +
-        //'            <button type="button" class="btn btn-black" id="registerButton">Register</button>\n' +
-        '        </form>\n' +
-        '    </div>\n' +
-        '</div>\n'
+        '    </div>';
     $("#user").html(s);
 }
