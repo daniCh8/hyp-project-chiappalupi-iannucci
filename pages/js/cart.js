fc9cf5556d5593ebbdd0e191fcbe8e66923175bd
Elena Iannucci <elena.iannucci@mail.polimi.it>
2019-07-04 13:22:14 +0200

    Added get cart function

--- pages/js/cart.js
+++ pages/js/cart.js
@@ -1,4 +1,139 @@
 "use strict";
+
+
+// This function retrieves items in the cart from the server and builds the UI accordingly
+function fetchItems() {
+    jQuery.ajax({
+        url: "https://hyp-2019-chiappalupi-iannucci.herokuapp.com/cart",
+        type: 'GET',
+        data: filter,
+        dataType: 'json',
+        success: (data) => {
+            console.log('ajax success');
+            fetchBook(data);
+        },
+        error: ()=>{
+            notifyerror("qualcosa è andato storto");
+        }
+    });
+}
+
+
 $(document).ready(() => {
-    getCartItems();
+
+    isUserLoggedIn(function(loggato) {
+        if (loggato) {
+            //alert("L'utente è loggato!");
+            fetchItems();
+        } else {
+            //alert("L'utente NON è loggato!");
+            window.location.replace("myaccount.html?");
+        }
+    });
+
 });
+
+
+function isUserLoggedIn(callBack) {
+    jQuery.ajax({
+        url: 'http://hyp-2019-chiappalupi-iannucci.herokuapp.com/user',
+        type: 'GET',
+        dataType: 'json',
+        credentials: 'same-origin',
+        Origin: "http://hyp-2019-chiappalupi-iannucci.herokuapp.com",
+        success: (result) => {
+            console.log('ajax success');
+            callBack(true);
+        },
+        error: (result) => {
+            callBack(false);
+            console.log('ajax error')
+            console.log(result.errorMessage)
+            notifyerror(result.errorMessage);
+        }
+    });
+}
+
+
+
+function fetchBook(prevData) {
+    for(var i = 0; i < prevData.length; i++){
+        var s = "https://hyp-2019-chiappalupi-iannucci.herokuapp.com/book/" + data[i].ISBN;
+        // Use the filter endpoint
+        jQuery.ajax({
+            url: s,
+            type: 'GET',
+            dataType: 'json',
+            success: (data) => {
+                console.log('ajax success');
+                var r = drawBook(data, prevData[i]);
+                $('#itemsshelf').append(r);
+            },
+            error: () => {
+                notifyerror("qualcosa è andato storto");
+            }
+        });
+    }
+}
+
+
+function drawBook(data, prevData) {
+    var s = '';
+            s = s + '<div class = "bookcontainer1">';
+        s = s + '<a class="booklink" href="book.html?name=' + data[0].name + '"> <div class="component">\n' +
+            '    <ul class="align">\n' +
+            '        <li>\n' +
+            '            <figure class=\'book fadeInDown\' >\n' +
+            '\n' +
+            '                <!-- Front -->\n' +
+            '\n' +
+            '                <ul class=\'hardcover_front\'>\n' +
+            '                    <li>\n' +
+            '                        <div class="coverDesign">\n' +
+            '                            \n' +
+            '<img src="' + data[0].pictureURL + '" style="width:160px ;height:220px;">' +
+            '                        </div>\n' +
+            '                    </li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+            '\n' +
+            '                <!-- Pages -->\n' +
+            '\n' +
+            '                <ul class=\'page\'>\n' +
+            '                    <li></li>\n' +
+            '                    <li style="align-content: center">\n' +
+            '<div class="container" style="padding: 0px; height: -webkit-fill-available; display: flex; justify-content:center; align-content:center;flex-direction:column;"> ' +
+            '<p style="margin-bottom: 2px"><b>' + data[0].name + '</b></p>';
+        for(var j = 0; j <data[0].authors.length; j++) {
+            s = s + '<p>' + data[0].authors[j] + '</p>'
+
+        }
+        s = s +
+            '<p style="margin-top: 10px"><i>' + data[0].theme + '</i></p>' +
+            '<p><i>' + data[0].genre + '</i></p>'+
+            '                    </div></li>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+            '\n' +
+            '                <!-- Back -->\n' +
+            '\n' +
+            '                <ul class=\'hardcover_back\'>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+            '                <ul class=\'book_spine\'>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+
+            '            </figure>\n' +
+            '        </li>\n' +
+            '    </ul>\n' +
+            '</a></div>';
+        s = s + '<p class = "paragraph-fav-best"> Quantity: ' + prevData.quantity + '</p>';
+        s = s + "</div>";
+
+    return s;
+}
---
+++ pages/js/index.js
@@ -0,0 +1,225 @@
+"use strict";
+
+// This function takes an array of books as input and returns the HTML necessary to display them.
+function drawBooks(data, int) {
+    var s = '';
+    for (var i = 0; i < data.length; i++) {
+        if(int==1){
+            s = s + '<div class = "bookcontainer1">';
+        }
+        if(int==2){
+            s = s + '<div class = "bookcontainer2">';
+        }
+        if(int==3){
+            s = s + '<div class = "bookcontainer3">';
+        }
+        s = s + '<a class="booklink" href="book.html?name=' + data[i].name + '"> <div class="component">\n' +
+            '    <ul class="align">\n' +
+            '        <li>\n' +
+            '            <figure class=\'book fadeInDown\' >\n' +
+            '\n' +
+            '                <!-- Front -->\n' +
+            '\n' +
+            '                <ul class=\'hardcover_front\'>\n' +
+            '                    <li>\n' +
+            '                        <div class="coverDesign">\n' +
+            '                            \n' +
+            '<img src="' + data[i].pictureURL + '" style="width:160px ;height:220px;">' +
+            '                        </div>\n' +
+            '                    </li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+            '\n' +
+            '                <!-- Pages -->\n' +
+            '\n' +
+            '                <ul class=\'page\'>\n' +
+            '                    <li></li>\n' +
+            '                    <li style="align-content: center">\n' +
+            '<div class="container" style="padding: 0px; height: -webkit-fill-available; display: flex; justify-content:center; align-content:center;flex-direction:column;"> ' +
+            '<p style="margin-bottom: 2px"><b>' + data[i].name + '</b></p>';
+        for(var j = 0; j <data[i].authors.length; j++) {
+            s = s + '<p>' + data[i].authors[j] + '</p>'
+
+        }
+        s = s +
+            '<p style="margin-top: 10px"><i>' + data[i].theme + '</i></p>' +
+            '<p><i>' + data[i].genre + '</i></p>'+
+            '                    </div></li>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+            '\n' +
+            '                <!-- Back -->\n' +
+            '\n' +
+            '                <ul class=\'hardcover_back\'>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+            '                <ul class=\'book_spine\'>\n' +
+            '                    <li></li>\n' +
+            '                    <li></li>\n' +
+            '                </ul>\n' +
+
+            '            </figure>\n' +
+            '        </li>\n' +
+            '    </ul>\n' +
+            '</a></div>';
+
+        if(int==1) {
+            s = s + '<button onclick="addToCart("' + data[i].ISBN + ', 1");" style="background-color: rgba(68,54,39,0.1);"> <div class="container" style="display: flex; flex-direction: column;"> <img src="svg/mbri-cart-add.svg" alt="">  <p> Add to cart </p> </div> </button>'
+        }
+        if(int==2){
+            s = s + '<button onclick="addToCart("' + data[i].ISBN + ', 1");" style="background-color: rgba(179,166,187,0.4);"> <div class="container" style="display: flex; flex-direction: column;"> <img src="svg/mbri-cart-add.svg" alt="">  <p> Add to cart </p> </div> </button>'
+        }
+        if(int==3){
+            s = s + '<button onclick="addToCart("' + data[i].ISBN + ', 1");" style="background-color: rgba(115,130,144,0.4);"> <div class="container" style="display: flex; flex-direction: column;"> <img src="svg/mbri-cart-add.svg" alt="">  <p> Add to cart </p> </div> </button>'
+        }
+        s = s + "</div>";
+    }
+
+    return s;
+}
+
+// This function retrieves books from the server and builds the UI accordingly
+function fetchBooks(filter) {
+    // Filter parameter is optional
+    if (filter){
+        // Use the filter endpoint
+        jQuery.ajax({
+            url: "https://hyp-2019-chiappalupi-iannucci.herokuapp.com/book/findBooksBy",
+            type: 'GET',
+            data: filter,
+            dataType: 'json',
+            success: (data) => {
+                console.log('ajax success');
+                var s = drawBooks(data, 1);
+                $('#bookshelf').html(s);
+            },
+            error: ()=>{
+                notifyerror("qualcosa è andato storto");
+            }
+        });
+
+    } else {
+        // If no filter was specified, let's get them all!
+        jQuery.ajax({
+            url: 'https://hyp-2019-chiappalupi-iannucci.herokuapp.com/book',
+            //TODO: parametrize url
+            type: 'GET',
+            dataType: 'json',
+            success: (data) => {
+                console.log('ajax success');
+                var s = drawBooks(data, 1);
+                $('#bookshelf').html(s);
+            },
+            error: ()=>{
+                notifyerror("qualcosa è andato storto");
+            }
+        });
+    }
+}
+
+function searchClick(){
+    var filter = {};
+    var genres = $("#bookgenre").val();
+    if (genres !== "") {
+        filter.genres=genres;
+    }
+    var themes = $("#booktheme").val();
+    if (themes !== "") {
+        filter.themes=themes;
+    }
+    var name = $("#booktitle").val();
+    if (name !== "") {
+        filter.name=name;
+    }
+    var author = $("#authorname").val();
+    if (author !== "") {
+        filter.author=author;
+    }
+
+
+    fetchBooks(filter);
+}
+
+function searchClickNavbar(){
+    var filter = {};
+
+    var name = $("#booktitleNavbar").val();
+    if (name !== "") {
+        filter.name=name;
+    }
+    var author = $("#authornameNavbar").val();
+    if (author !== "") {
+        filter.author=author;
+    }
+
+
+    fetchBooks(filter);
+}
+
+$(document).ready(() => {
+
+    var filter = {};
+
+    //Checking if the client has specified any filter parameter
+    try {
+        var querystring = location.search;
+        var subquerystring = querystring.substr(1);
+        var keyvalyepairs = subquerystring.split('&');
+        $.each(keyvalyepairs, function(i, kv){
+            var key = kv.split('=')[0];
+            var value = kv.split('=')[1];
+            value = value.replace(/%20/g, ' ');
+            console.log(value);
+            filter[key]=value;
+        });
+    } catch{
+        //TODO:
+    }
+    console.log(filter);
+    $("#searchbutton").click(searchClick);
+    $("#searchbuttonSmall").click(searchClick);
+    $("#searchbuttonNavbarSmall").click(searchClickNavbar);
+
+    // As soon as the page loads, load all the books
+    fetchBooks(filter);
+    favouriteReading();
+    bestsellers();
+
+});
+
+function favouriteReading(){
+    jQuery.ajax({
+        url: 'https://hyp-2019-chiappalupi-iannucci.herokuapp.com/book/getBestsellers',
+        //TODO: parametrize url
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            console.log('ajax success');
+            var s = drawBooks(data, 2);
+            $('#bookshelfFav').html(s);
+        },
+        error: ()=>{
+            notifyerror("qualcosa è andato storto");
+        }
+    });
+}
+
+function bestsellers(){
+    jQuery.ajax({
+        url: 'https://hyp-2019-chiappalupi-iannucci.herokuapp.com/book/getFavouriteBooks',
+        //TODO: parametrize url
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            console.log('ajax success');
+            var s = drawBooks(data, 3);
+            $('#bookshelfBest').html(s);
+        },
+        error: ()=>{
+            notifyerror("qualcosa è andato storto");
+        }
+    });
+}
